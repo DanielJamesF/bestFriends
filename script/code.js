24cc6fc123dd4e324be7bc61cbc22b7f29ad6e8c
DanielJamesF <fredericksd17@gmail.com>
2022-06-07 10:50:45 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,12 @@
+let bestFriends = [
+    'Abdus-Samad', 'Bulelani', 'Yaseen', 'Cassidy', 'Clayton', 'Daniel', 'Jared', 'Muddathir', 'Reagan', 'James'
+];
+
+let div = document.getElementById('bestFriendss');
+
+// bestFriends.forEach( (e) => {
+//     console.log(e);
+// });
+
+console.log(bestFriends[bestFriends.length/2]);
+console.log(bestFriends[bestFriends.length-1]);
